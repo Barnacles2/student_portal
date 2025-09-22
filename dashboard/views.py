84f52c155fae02c952ec28b16469af7eecab429d
Kimberly <kimberly.oware@strathmore.edu>
2025-09-22 13:54:35 +0300

    created home and about

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+def home(request):
+    return render(request, 'dashboard/home.html')
+
+    def about(request):
+        return render(request, 'dashboard/about.html')
+
 # Create your views here.
